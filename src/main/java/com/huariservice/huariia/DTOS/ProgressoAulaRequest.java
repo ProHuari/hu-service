5ab826a222d87d4790fc12bee8a1a052db5fd3bb
l3ssinhy <lucas.050409@gmail.com>
2026-08-30 20:50:56 -0300

    feat: Criação de ProgressoAulaRequest

---
+++ src/main/java/com/huariservice/huariia/DTOS/ProgressoAulaRequest.java
@@ -0,0 +1,21 @@
+package com.huariservice.huariia.DTOS;
+
+import com.huariservice.huariia.entities.Aulas;
+import com.huariservice.huariia.entities.Usuario;
+import com.huariservice.huariia.entities.enums.StatusAula;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProgressoAulaRequest {
+
+    private StatusAula statusAula;
+    private LocalDateTime conclusao;
+    private Usuario usuario;
+    private Aulas aulas;
+}
