bd1abd7e1e5d622de15db067b39c432fbd87436f
l3ssinhy <lucas.050409@gmail.com>
2026-08-30 19:14:11 -0300

    feat: Adição de @Data, @AllArgsConstructor e @NoArgsConstructor na entidade Usuario

--- src/main/java/com/huariservice/huariia/entities/Usuario.java
+++ src/main/java/com/huariservice/huariia/entities/Usuario.java
@@ -2,13 +2,19 @@ package com.huariservice.huariia.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Size(min = 1, max = 100, message = "O nome deve ter entre 1 e 100 caracteres")
     @Column(length = 100,nullable = false)
@@ -22,61 +28,4 @@ public class Usuario {
     @Column(name = "Tipo_Perfil")
     private TipoPerfil tipoPerfil;
 
-    public Usuario() {
-    }
-
-    public Usuario(String email, String nome, String senha, TipoPerfil tipoPerfil) {
-        this.email = email;
-        this.nome = nome;
-        this.senha = senha;
-        this.tipoPerfil = tipoPerfil;
-    }
-
-    public Usuario(long id, String nome, String email, String senha, TipoPerfil tipoPerfil) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.tipoPerfil = tipoPerfil;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public TipoPerfil getTipoPerfil() {
-        return tipoPerfil;
-    }
-
-    public void setTipoPerfil(TipoPerfil tipoPerfil) {
-        this.tipoPerfil = tipoPerfil;
-    }
 }
