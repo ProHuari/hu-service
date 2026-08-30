acd663dfc2fba1b98d71aafe145744da011c4cb0
l3ssinhy <lucas.050409@gmail.com>
2026-08-30 20:34:09 -0300

    feat: Criação de CursosResponse

---
+++ src/main/java/com/huariservice/huariia/DTOS/CursosResponse.java
@@ -0,0 +1,20 @@
+package com.huariservice.huariia.DTOS;
+
+import com.huariservice.huariia.entities.Autores;
+import com.huariservice.huariia.entities.Categorias;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CursosResponse {
+
+    private Long id;
+    private String titulos;
+    private String descricao;
+    private String urlVideo;
+    private Categorias categoria;
+    private Autores autores;
+}
