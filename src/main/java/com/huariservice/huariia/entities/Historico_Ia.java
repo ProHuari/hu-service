a11e6db52c7e1e2e67fb9604a369b60340c23a50
l3ssinhy <lucas.050409@gmail.com>
2026-08-30 18:59:13 -0300

    feat: Adição de @Data, @AllArgsConstructor e @NoArgsConstructor na entidade Historico_Ia

--- src/main/java/com/huariservice/huariia/entities/Historico_Ia.java
+++ src/main/java/com/huariservice/huariia/entities/Historico_Ia.java
@@ -1,14 +1,20 @@
 package com.huariservice.huariia.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "historicoIA")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Historico_Ia {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(columnDefinition = "TEXT",nullable = false)
     private String pergunta;
@@ -20,61 +26,4 @@ public class Historico_Ia {
     @JoinColumn(name = "usuario_id",nullable = false)
     private Usuario usuario;
 
-    public Historico_Ia() {
-    }
-
-    public Historico_Ia( String pergunta, String resposta, LocalDateTime dataConsulta, Usuario usuario) {
-        this.pergunta = pergunta;
-        this.resposta = resposta;
-        this.dataConsulta = dataConsulta;
-        this.usuario = usuario;
-    }
-
-    public Historico_Ia(Long id, String pergunta, String resposta, LocalDateTime dataConsulta, Usuario usuario) {
-        this.id = id;
-        this.pergunta = pergunta;
-        this.resposta = resposta;
-        this.dataConsulta = dataConsulta;
-        this.usuario = usuario;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPergunta() {
-        return pergunta;
-    }
-
-    public void setPergunta(String pergunta) {
-        this.pergunta = pergunta;
-    }
-
-    public String getResposta() {
-        return resposta;
-    }
-
-    public void setResposta(String resposta) {
-        this.resposta = resposta;
-    }
-
-    public LocalDateTime getDataConsulta() {
-        return dataConsulta;
-    }
-
-    public void setDataConsulta(LocalDateTime dataConsulta) {
-        this.dataConsulta = dataConsulta;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
 }
