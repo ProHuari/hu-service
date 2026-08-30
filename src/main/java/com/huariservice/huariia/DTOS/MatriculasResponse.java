db2f42f728babd26db61dfeff895fcef8ac6efa2
l3ssinhy <lucas.050409@gmail.com>
2026-08-30 20:40:25 -0300

    feat: Criação de MatriculasResponse

---
+++ src/main/java/com/huariservice/huariia/DTOS/MatriculasResponse.java
@@ -0,0 +1,22 @@
+package com.huariservice.huariia.DTOS;
+
+import com.huariservice.huariia.entities.Cursos;
+import com.huariservice.huariia.entities.Usuario;
+import com.huariservice.huariia.entities.enums.StatusMT;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MatriculasResponse {
+
+    private Long id;
+    private LocalDate dtMatricula;
+    private StatusMT statusMT;
+    private Usuario usuario;
+    private Cursos cursos;
+}
