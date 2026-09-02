b68ef6f4e06e46eb2b47e57384b335ff2c674ed4
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-02 13:59:38 -0300

    feat: colocando as anotetion no DTO do Historico_IAAulaRequest

--- src/main/java/com/huariservice/huariia/DTOS/Historico_IaRequest.java
+++ src/main/java/com/huariservice/huariia/DTOS/Historico_IaRequest.java
@@ -1,6 +1,8 @@
 package com.huariservice.huariia.DTOS;
 
 import com.huariservice.huariia.entities.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +13,11 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Historico_IaRequest {
-
+    @NotBlank(message = "A pergunta é obrigatória")
     private String pergunta;
+    @NotBlank(message = "A resposta é obrigatória")
     private String resposta;
     private LocalDateTime dataConsulta;
+    @NotNull(message = "O usuário é obrigatório")
     private Usuario usuario;
 }
