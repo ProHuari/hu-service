3ab815d36e8088dcdb47222502a201b5ee50a6ad
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-02 13:52:16 -0300

    feat: colocando as anotetion no DTO do AulasRequest

--- src/main/java/com/huariservice/huariia/DTOS/AulasRequest.java
+++ src/main/java/com/huariservice/huariia/DTOS/AulasRequest.java
@@ -1,6 +1,9 @@
 package com.huariservice.huariia.DTOS;
 
 import com.huariservice.huariia.entities.Modulo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +12,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AulasRequest {
-
+    @NotBlank(message = "O título é obrigatório")
+    @Size(max = 155)
     private String titulos;
+    @NotBlank(message = "A descrição é obrigatória")
     private String descricao;
+    @NotBlank(message = "A URL do vídeo é obrigatória")
+    @Size(max = 254)
     private String urlVideo;
+    @NotNull(message = "A ordem é obrigatória")
     private Integer ordem;
     private Integer duracaoEmMinutos;
+    @NotNull(message = "O módulo é obrigatório")
     private Modulo modulo;
 }
