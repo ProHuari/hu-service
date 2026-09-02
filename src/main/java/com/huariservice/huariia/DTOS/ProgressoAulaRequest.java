36859d41c80683a5df6a3ef76a65ede81a2c5b40
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-02 13:56:31 -0300

    feat: colocando as anotetion no DTO do ProgresoAulaRequest

--- src/main/java/com/huariservice/huariia/DTOS/ProgressoAulaRequest.java
+++ src/main/java/com/huariservice/huariia/DTOS/ProgressoAulaRequest.java
@@ -3,6 +3,7 @@ package com.huariservice.huariia.DTOS;
 import com.huariservice.huariia.entities.Aulas;
 import com.huariservice.huariia.entities.Usuario;
 import com.huariservice.huariia.entities.enums.StatusAula;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +14,11 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProgressoAulaRequest {
-
+    @NotNull(message = "O status é obrigatório")
     private StatusAula statusAula;
     private LocalDateTime conclusao;
+    @NotNull(message = "O usuário é obrigatório")
     private Usuario usuario;
+    @NotNull(message = "A aula é obrigatória")
     private Aulas aulas;
 }
