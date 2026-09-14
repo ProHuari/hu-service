99db5e3f8b1029813604ec63fa98fa461b967a0c
l3ssinhy <lucas.050409@gmail.com>
2026-09-14 09:11:23 -0300

    feat: cria ModuloRepository para persistência de dados

---
+++ src/main/java/com/huariservice/huariia/repositories/ModuloRepository.java
@@ -0,0 +1,9 @@
+package com.huariservice.huariia.repositories;
+
+import com.huariservice.huariia.entities.Modulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModuloRepository extends JpaRepository<Modulo, Long> {
+}
