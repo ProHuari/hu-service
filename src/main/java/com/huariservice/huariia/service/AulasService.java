17cf284a2a1b7bce3425ee1592cf9902c5612e3f
l3ssinhy <lucas.050409@gmail.com>
2026-09-14 10:07:11 -0300

    feat: mAulasService método post

--- src/main/java/com/huariservice/huariia/service/AulasService.java
+++ src/main/java/com/huariservice/huariia/service/AulasService.java
@@ -1,7 +1,35 @@
 package com.huariservice.huariia.service;
 
+import com.huariservice.huariia.DTOS.AulasRequest;
+import com.huariservice.huariia.entities.Aulas;
+import com.huariservice.huariia.entities.Modulo;
+import com.huariservice.huariia.repositories.AulasRepository;
+import com.huariservice.huariia.repositories.ModuloRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AulasService {
+
+    private final AulasRepository aulasRepository;
+    private final ModuloRepository moduloRepository;
+
+    public AulasService(AulasRepository aulasRepository, ModuloRepository moduloRepository) {
+        this.aulasRepository = aulasRepository;
+        this.moduloRepository = moduloRepository;
+    }
+
+    public AulasRequest pubAula(AulasRequest request) {
+        Aulas aula = new Aulas();
+        aula.setTitulos(request.getTitulos());
+        aula.setDescricao(request.getDescricao());
+        aula.setUrlVideo(request.getUrlVideo());
+        aula.setOrdem(request.getOrdem());
+        aula.setDuracaoEmMinutos(request.getDuracaoEmMinutos());
+
+        Modulo modulo = moduloRepository.findById(request.getModulo().getId()).orElseThrow();
+        aula.setModulo(modulo);
+
+        aulasRepository.save(aula);
+        return request;
+    }
 }
