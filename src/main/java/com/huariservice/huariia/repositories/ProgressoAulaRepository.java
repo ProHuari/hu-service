43229fd6aefa18b82af32cdecc5245fa52a845e9
l3ssinhy <lucas.050409@gmail.com>
2026-09-14 09:13:53 -0300

    feat: cria ProgressoAulaRepository para persistência de dados

---
+++ src/main/java/com/huariservice/huariia/repositories/ProgressoAulaRepository.java
@@ -0,0 +1,9 @@
+package com.huariservice.huariia.repositories;
+
+import com.huariservice.huariia.entities.ProgresoAula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProgressoAulaRepository extends JpaRepository<ProgresoAula, Long> {
+}
