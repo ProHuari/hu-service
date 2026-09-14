3aaed14b97bca32d3e3739cab3f730487b633531
l3ssinhy <lucas.050409@gmail.com>
2026-09-14 10:22:17 -0300

    feat: AutoresService método get

--- src/main/java/com/huariservice/huariia/service/AutoresService.java
+++ src/main/java/com/huariservice/huariia/service/AutoresService.java
@@ -1,9 +1,12 @@
 package com.huariservice.huariia.service;
 
 import com.huariservice.huariia.DTOS.AutoresRequest;
+import com.huariservice.huariia.DTOS.AutoresResponse;
 import com.huariservice.huariia.entities.Autores;
 import com.huariservice.huariia.repositories.AutoresRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AutoresService {
@@ -21,5 +24,13 @@ public class AutoresService {
 
         autoresRepository.save(autor);
         return request;
+    }
+
+    public List<AutoresResponse> mostrarAutores() {
+        return autoresRepository.findAll().stream().map(autor -> new AutoresResponse(
+                autor.getId(),
+                autor.getNomeCanal(),
+                autor.getLinkCanal()
+        )).toList();
     }
 }
