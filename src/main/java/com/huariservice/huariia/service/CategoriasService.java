593c7464043c017289d13621b1a854693cabcef3
l3ssinhy <lucas.050409@gmail.com>
2026-09-14 10:28:39 -0300

    feat: cria CategoriasService método post e get

---
+++ src/main/java/com/huariservice/huariia/service/CategoriasService.java
@@ -0,0 +1,36 @@
+package com.huariservice.huariia.service;
+
+import com.huariservice.huariia.DTOS.CategoriasRequest;
+import com.huariservice.huariia.DTOS.CategoriasResponse;
+import com.huariservice.huariia.entities.Categorias;
+import com.huariservice.huariia.repositories.CategoriasRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriasService {
+
+    private final CategoriasRepository categoriasRepository;
+
+    public CategoriasService(CategoriasRepository categoriasRepository) {
+        this.categoriasRepository = categoriasRepository;
+    }
+
+    public CategoriasRequest pubCategoria(CategoriasRequest request) {
+        Categorias categoria = new Categorias();
+        categoria.setNome(request.getNome());
+        categoria.setDescricao(request.getDescricao());
+
+        categoriasRepository.save(categoria);
+        return request;
+    }
+
+    public List<CategoriasResponse> mostrarCategorias() {
+        return categoriasRepository.findAll().stream().map(categoria -> new CategoriasResponse(
+                categoria.getId(),
+                categoria.getNome(),
+                categoria.getDescricao()
+        )).toList();
+    }
+}
