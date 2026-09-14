46399140c2ac7b6ff2f6eda1026e60faa06e7407
LUCAS FARIAS LESSA <lucas.lessa4@senaisp.edu.br>
2026-09-14 09:02:51 -0300

    feat: cria CursosRepository para persistencia de dados

---
+++ src/main/java/com/huariservice/huariia/repositories/CursosRepository.java
@@ -0,0 +1,9 @@
+package com.huariservice.huariia.repositories;
+
+import com.huariservice.huariia.entities.Cursos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursosRepository extends JpaRepository<Cursos, Long> {
+}
