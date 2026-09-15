7374fa65fdaaf15b4d9719481803e19e271b9ada
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-15 10:59:52 -0300

    feat: coloando o metodo pumodulos

--- src/main/java/com/huariservice/huariia/service/ModulosService.java
+++ src/main/java/com/huariservice/huariia/service/ModulosService.java
@@ -1,7 +1,32 @@
 package com.huariservice.huariia.service;
 
+import com.huariservice.huariia.DTOS.ModulosRequest;
+import com.huariservice.huariia.entities.Cursos;
+import com.huariservice.huariia.entities.Modulo;
+import com.huariservice.huariia.repositories.CursosRepository;
+import com.huariservice.huariia.repositories.ModuloRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ModulosService {
+    private final ModuloRepository moduloRepository;
+    private final CursosRepository cursosRepository;
+
+
+    public ModulosService(ModuloRepository moduloRepository, CursosRepository cursosRepository) {
+        this.moduloRepository = moduloRepository;
+        this.cursosRepository = cursosRepository;
+    }
+    public ModulosRequest pubModulo(ModulosRequest request) {
+        Modulo modulo = new Modulo();
+        modulo.setTitulo(request.getTitulo());
+        modulo.setDescricao(request.getDescricao());
+        modulo.setOrdem(request.getOrdem());
+
+        Cursos cursos = cursosRepository.findById(request.getCursos().getId()).orElseThrow();
+        modulo.setCursos(cursos);
+
+        moduloRepository.save(modulo);
+        return request;
+    }
 }
