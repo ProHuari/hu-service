b49375180d0992f8ed128321371017c09ee435b2
l3ssinhy <lucas.050409@gmail.com>
2026-09-15 17:01:46 -0300

    feat: UsuarioService método put

--- src/main/java/com/huariservice/huariia/service/UsuarioService.java
+++ src/main/java/com/huariservice/huariia/service/UsuarioService.java
@@ -35,4 +35,16 @@ public class UsuarioService {
                 usuario.getTipoPerfil()
         )).toList();
     }
+
+    public String mudarUsuario(Long id, UsuarioRequest usuarioAlterado) {
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
+
+        usuario.setNome(usuarioAlterado.getNome());
+        usuario.setEmail(usuarioAlterado.getEmail());
+        usuario.setSenha(usuarioAlterado.getSenha());
+        usuario.setTipoPerfil(usuarioAlterado.getTipoPerfil());
+
+        usuarioRepository.save(usuario);
+        return "Usuário alterado";
+    }
 }
