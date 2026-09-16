f4c9c8bf057d3647a34b5dd46f0a8e3f171cf842
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-16 18:41:24 -0300

    feat: adicionado a aula controler o get  mapping

--- src/main/java/com/huariservice/huariia/controller/aulasservice.java
+++ src/main/java/com/huariservice/huariia/controller/aulasservice.java
@@ -1,12 +1,12 @@
 package com.huariservice.huariia.controller;
 
 import com.huariservice.huariia.DTOs.AulasRequest;
+import com.huariservice.huariia.DTOs.AulasResponse;
 import com.huariservice.huariia.service.AulasService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("aulas")
@@ -26,4 +26,13 @@ public class aulasservice {
 
         return ResponseEntity.ok(aula);
     }
+    @GetMapping
+    public ResponseEntity<List<AulasResponse>> mostrarAulas() {
+
+        List<AulasResponse> aulas = aulasService.mostrarAulas();
+
+        return ResponseEntity.ok(aulas);
+    }
+
+
 }
