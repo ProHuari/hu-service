15951bb4123ee9a5cc3e89626fa4df9c0daceecf
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-16 19:01:52 -0300

    feat: adicionado a CursosController  o getmapping

--- src/main/java/com/huariservice/huariia/controller/CursosController.java
+++ src/main/java/com/huariservice/huariia/controller/CursosController.java
@@ -1,13 +1,13 @@
 package com.huariservice.huariia.controller;
 
 import com.huariservice.huariia.DTOs.CursosRequest;
+import com.huariservice.huariia.DTOs.CursosResponse;
 import com.huariservice.huariia.service.CursosService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cursos")
@@ -21,5 +21,10 @@ public class CursosController {
     public ResponseEntity<CursosRequest> criarCurso(@RequestBody CursosRequest request) {
         CursosRequest novoCurso = cursosService.pubCurso(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoCurso);
+    }
+    @GetMapping
+    public ResponseEntity<List<CursosResponse>> listarCursos() {
+        List<CursosResponse> cursos = cursosService.mostrarCursos();
+        return ResponseEntity.ok(cursos);
     }
 }
