2fe692816acdfa039829111648eaddb31679350d
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-16 19:24:29 -0300

    feat: adicionado a ProgresoAulasontroller  o getmapping

--- src/main/java/com/huariservice/huariia/controller/ProgresoAulaController.java
+++ src/main/java/com/huariservice/huariia/controller/ProgresoAulaController.java
@@ -1,13 +1,13 @@
 package com.huariservice.huariia.controller;
 
 import com.huariservice.huariia.DTOs.ProgressoAulaRequest;
+import com.huariservice.huariia.DTOs.ProgressoAulaResponse;
 import com.huariservice.huariia.service.ProgressoAulaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ProgresoAula")
@@ -23,5 +23,9 @@ public class ProgresoAulaController {
         ProgressoAulaRequest novoProgresso = progressoAulaService.pubProgressoAula(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoProgresso);
     }
-
+    @GetMapping
+    public ResponseEntity<List<ProgressoAulaResponse>> listarProgressoAula() {
+        List<ProgressoAulaResponse> progressos = progressoAulaService.mostrarProgressoAula();
+        return ResponseEntity.ok(progressos);
+    }
 }
