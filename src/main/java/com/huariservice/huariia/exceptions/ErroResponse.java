91c7e15a33462eebff0982c12442f1fede5379ac
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-16 19:44:20 -0300

    feat: em exceptions foi feito o erroresponse

--- src/main/java/com/huariservice/huariia/exceptions/ErroResponse.java
+++ src/main/java/com/huariservice/huariia/exceptions/ErroResponse.java
@@ -1,4 +1,18 @@
 package com.huariservice.huariia.exceptions;
 
-public record ErroResponse() {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ErroResponse(LocalDateTime timestamp,
+                           int status,
+                           String erro,
+                           String mensagem,
+                           List<String> detalhes) {
+    public ErroResponse(int status, String erro, String mensagem) {
+        this(LocalDateTime.now(), status, erro, mensagem, null);
+    }
+
+    public ErroResponse(int status, String erro, String mensagem, List<String> detalhes) {
+        this(LocalDateTime.now(), status, erro, mensagem, detalhes);
+    }
 }
