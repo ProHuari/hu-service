ef1c7e9ad90a548b317c3ae098b63818ef86d5ab
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-16 19:19:29 -0300

    feat: adicionado a ModulsoController  o gettmapping

--- src/main/java/com/huariservice/huariia/controller/ModulosController.java
+++ src/main/java/com/huariservice/huariia/controller/ModulosController.java
@@ -1,13 +1,13 @@
 package com.huariservice.huariia.controller;
 
 import com.huariservice.huariia.DTOs.ModulosRequest;
+import com.huariservice.huariia.DTOs.ModulosResponse;
 import com.huariservice.huariia.service.ModulosService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/modulos")
@@ -17,11 +17,14 @@ public class ModulosController {
     public ModulosController(ModulosService modulosService) {
         this.modulosService = modulosService;
     }
-
-
     @PostMapping
     public ResponseEntity<ModulosRequest> criarModulo(@RequestBody ModulosRequest request) {
         ModulosRequest novoModulo = modulosService.pubModulo(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoModulo);
+    }
+    @GetMapping
+    public ResponseEntity<List<ModulosResponse>> listarModulos() {
+        List<ModulosResponse> modulos = modulosService.mostrarModulos();
+        return ResponseEntity.ok(modulos);
     }
 }
