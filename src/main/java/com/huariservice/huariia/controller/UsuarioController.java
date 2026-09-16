293cd937a9d693ffddfb75acdd66a7485fe53978
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-16 19:30:00 -0300

    feat: adicionado a ProgresoAulasontroller  o deletemapping

---
+++ src/main/java/com/huariservice/huariia/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.huariservice.huariia.controller;
+
+import com.huariservice.huariia.DTOs.UsuarioRequest;
+import com.huariservice.huariia.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Usuarios")
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+    @PostMapping
+    public ResponseEntity<UsuarioRequest> criarUsuario(@RequestBody UsuarioRequest request) {
+        UsuarioRequest novoUsuario = usuarioService.pubUsuario(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+    }
+}
