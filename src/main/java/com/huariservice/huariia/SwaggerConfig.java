77506a7abc9a8b1524830839b0cacb68a5f53a5e
l3ssinhy <lucas.050409@gmail.com>
2026-09-21 10:53:50 -0300

    feat: adicionar configuração do swagger

--- src/main/java/com/huariservice/huariia/SwaggerConfig.java
+++ src/main/java/com/huariservice/huariia/SwaggerConfig.java
@@ -1,4 +1,22 @@
 package com.huariservice.huariia;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
 public class SwaggerConfig {
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+            return new OpenAPI()
+                    .info(new Info()
+                            .title("Huari.ia REST API")
+                            .description("Documentação da API Huari.ia")
+                            .version("1.0.0")
+                            .license(new License()
+                                    .name("Apache License Version 2.0")
+                                    .url("https://www.apache.org/licenses/LICENSE-2.0")));
+        }
 }
