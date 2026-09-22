3a1f9b0ac05924934959fa777d46f50966177a99
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-21 22:51:42 -0300

    refactor(autor): converte AutoresRequest de classe para record

--- src/main/java/com/huariservice/huariia/DTOs/AutoresRequest.java
+++ src/main/java/com/huariservice/huariia/DTOs/AutoresRequest.java
@@ -2,19 +2,10 @@ package com.huariservice.huariia.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class AutoresRequest {
 
-    @NotBlank(message = "O nome do canal é obrigatório")
-    @Size(max = 100)
-    private String nomeCanal;
-    @NotBlank(message = "O link do canal é obrigatório")
-    @Size(max = 254)
-    private String linkCanal;
-}
+
+public record AutoresRequest(
+        @NotBlank @Size(max = 100) String nomeCanal,
+        @NotBlank @Size(max = 254) String linkCanal
+) {}
